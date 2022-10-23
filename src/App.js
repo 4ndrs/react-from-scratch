58fc60370f17d15b412a4ca0fcfec22cb2e561f5
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-10-23 09:07:53 +0200

    Change component class to function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, World!</h1>
-      </div>
-    );
-  }
+export default function App() {
+  return <h1>Hello, World!</h1>;
 }
-
-export default App;
